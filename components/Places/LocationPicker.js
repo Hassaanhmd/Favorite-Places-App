e718cea2e150d38a3638633868d6359170bbdef2
Hazey <hassaanhmd@gmail.com>
2022-08-13 11:09:54 +0200

    added interative map where user can select a location where a pin will drop

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Alert, View, StyleSheet, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import {
   getCurrentPositionAsync,
   useForegroundPermissions,
@@ -13,6 +14,7 @@ import { getMapPreview } from '../../util/location';
 function LocationPicker() {
   const [pickedLocation, setPickedLocation] = useState();
 
+  const navigation = useNavigation();
   const [locationPermissionInformation, requestPermission] =
     useForegroundPermissions();
 
@@ -50,7 +52,9 @@ function LocationPicker() {
     });
   }
 
-  function pickOnMapHandler() {}
+  function pickOnMapHandler() {
+    navigation.navigate('Map');
+  }
 
   let locationPreview = <Text>No location picked yet.</Text>;
 
@@ -91,7 +95,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: Colors.primary100,
     borderRadius: 4,
-    overflow: 'hidden'
+    overflow: 'hidden',
   },
   actions: {
     flexDirection: 'row',
